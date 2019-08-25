86a8e999459ba3cba593efa0b3519c2f51bfce9f
unknown <ecimoramora@gmail.com>
2019-08-25 14:50:38 +0700

    salah

--- modal.php
+++ modal.php
@@ -27,7 +27,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button class='btn btn-info'>Test</button>
+       
       </div>
     </div>
   </div>
