f0aac9d23bbdabeb2991fbe0cdfdd7c5a259fbbd
unknown <ecimoramora@gmail.com>
2019-08-25 11:04:46 +0700

    first

---
+++ testimoni.php
@@ -0,0 +1,87 @@
+<div class='testimoni' id='testimoni'>
+
+<div class='row'>
+          <div class='col-sm-3'>
+            <div class='panel panel-info'>
+              <div class='panel-head'></div>
+              <div class='panel-body'>
+              <div>
+                <img src="./assets/img/faces/avatar.jpg" class="img-raised rounded-circle img-fluid" alt="">
+                </div>
+              <div class="blockquote undefined">
+                <p>
+                  I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.
+                </p>
+                <small>
+                  Kanye West, Musician
+                </small>
+              </div>
+              </div>
+            
+            </div>
+          </div>
+          <div class='col-sm-3'>
+            <div class='panel panel-info'>
+              <div class='panel-head'></div>
+              <div class='panel-body'>
+              <div>
+                <img src="./assets/img/faces/avatar.jpg" class="img-raised rounded-circle img-fluid" alt="">
+                </div>
+              <div class="blockquote undefined">
+                <p>
+                  I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.
+                </p>
+                <small>
+                  Kanye West, Musician
+                </small>
+              </div>
+              </div>
+         
+             
+            </div>
+          </div>
+          <div class='col-sm-3'>
+            <div class='panel panel-info'>
+              <div class='panel-head'></div>
+              <div class='panel-body'>
+              <div>
+                <img src="./assets/img/faces/avatar.jpg" class="img-raised rounded-circle img-fluid" alt="">
+                </div>
+              <div class="blockquote undefined">
+                <p>
+                  I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.
+                </p>
+                <small>
+                  Kanye West, Musician
+                </small>
+              </div>
+              </div>
+             
+          
+            </div>
+          </div>
+          <div class='col-sm-3'>
+            <div class='panel panel-info'>
+              <div class='panel-head'></div>
+              <div class='panel-body'>
+              <div>
+                <img src="./assets/img/faces/avatar.jpg" class="img-raised rounded-circle img-fluid" alt="">
+                </div>
+              <div class="blockquote undefined">
+                <p>
+                  I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.
+                </p>
+                <small>
+                  Kanye West, Musician
+                </small>
+              </div>
+              </div>
+              
+            
+            </div>
+          </div>
+        </div>
+
+        
+
+</div>
