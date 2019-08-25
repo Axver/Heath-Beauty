9dc80358e6156c364bebf129fad8fbe20746e95c
unknown <ecimoramora@gmail.com>
2019-08-25 17:04:59 +0700

    Tambahan Logo Tiens Syariah

--- penjelasan.php
+++ penjelasan.php
@@ -1,3 +1,11 @@
+<div class="thumbnail">
+      
+        <img src="image/tiens-logo-versi-2.gif" alt="Fjords" style="width:100%">
+        <div class="caption">
+          
+        </div>
+      
+    </div>
 <h3><b style='color:#542772'>Apa itu Spirulina?</b></h3>
 <p>Sudah tahu masker spirulina? Itu lho, masker yang warnanya hijau dan bikin kulit bersih setelah menggunakannya. Tetapi, bukan cuma itu saja manfaat dari si masker hijau ini
 </p>
